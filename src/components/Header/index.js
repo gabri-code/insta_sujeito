4c154f0ce1aabfd1d6de7751224f1aaf87fc6b4c
Gabriel Ferreira <gabriellf.14@hotmail.com>
2022-12-19 10:31:23 -0300

    create header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+// Icon.loadFont();
 
 export default function Header() {
+  const handlePhoto = () => {
+    alert('Vai tirar foto.');
+  };
+
+  const handleSend = () => {
+    alert('Vai enviar mensagem.');
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        <Icon name="camera" color="#010203" />
-      </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={handlePhoto}>
+        <FeatherIcon name="camera" color="rgb(38,38,38)" size={25} />
+      </TouchableWithoutFeedback>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Instagram</Text>
+      </View>
+      <TouchableWithoutFeedback onPress={handleSend}>
+        <FeatherIcon
+          name="send"
+          color="rgb(38,38,38)"
+          size={25}
+          style={styles.sendIcon}
+        />
+      </TouchableWithoutFeedback>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 55,
+    height: 60,
+    width: '100%',
     alignItems: 'center',
+    justifyContent: 'space-between',
     flexDirection: 'row',
     paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.05)',
+  },
+  titleContainer: {
+    justifyContent: 'center',
+  },
+  title: {
+    fontFamily: 'Billabong',
+    fontSize: 30,
+    color: 'rgb(38,38,38)',
+    lineHeight: 70,
+  },
+  sendIcon: {
+    transform: [{ rotate: '15deg' }],
   },
 });
